ba8da9db427bbda0e2c199c173ff837d6e9428e6
naomi <naomi.emma.spencer@gmail.com>
2016-10-09 08:57:24 +0100

    Added menu method and test

---
+++ lib/menu.rb
@@ -0,0 +1,11 @@
+class Menu
+
+  def initialize
+    @menu = { 'chicken' => 10, 'fish' => 12, 'veg' => 8}
+        end
+
+private
+
+attr_reader :menu
+
+end
---
+++ spec/menu_spec.rb
@@ -0,0 +1,12 @@
+require 'menu'
+
+describe Menu do
+  describe Menu do
+  subject(:menu) {described_class.new(item, price)}
+  let(:item) {{:chicken => 10, :fish => 12, :veg => 8 }}
+  end
+
+    it 'prints the menu' do
+      @menu
+    end
+end
